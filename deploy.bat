a30b3dd3c2daf9dbbd01194ce76aa9b794be8c28
Thomas Gorr <thomas.gorr@inform-software.com>
2019-01-23 20:35:16 +0100

    Updates environment settings

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,3 @@
 REM Deploy demo script
 
-cp --recursive --verbose ./build/* C:/Users/tgorr/Documents/Qlik/Sense/Extensions/timeScatter
+cp --recursive --verbose ./build/* C:/Users/tgorr/Documents/Qlik/Sense/Extensions/FlappyData
