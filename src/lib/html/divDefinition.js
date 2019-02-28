c2331925a1106b22e222fee82d14163ba6f21332
Thomas Gorr <thomas.gorr@inform-software.com>
2019-02-28 10:49:29 +0100

    Fixes code

--- src/lib/html/divDefinition.js
+++ src/lib/html/divDefinition.js
@@ -1,5 +1,4 @@
-
-export function     defineHTML($, $element) {
+export function defineHTML($, $element) {
     $element.empty();
     let html = "<div id='flappyData' style='height:80%; width:100%'></div>";
     $element.html(html);
